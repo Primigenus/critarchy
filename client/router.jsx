3afc3d3e1c207d49f93eebeed98a55507b6bca85
dierat <diedra.rater@gmail.com>
2016-04-13 16:45:06 -0500

    adds ArtList component for /art route

--- client/router.jsx
+++ client/router.jsx
@@ -3,10 +3,35 @@ import {mount} from 'react-mounter';
 
 import MainLayout from './layout.jsx';
 import Home from '/imports/ui/Home.jsx';
+import ArtList from '/imports/ui/ArtList.jsx';
 
 
 
 FlowRouter.route("/", {
+  action() {
+    mount(MainLayout, {
+      content: <Home />
+    });
+  }
+});
+
+FlowRouter.route("/crits", {
+  action() {
+    mount(MainLayout, {
+      content: <Home />
+    });
+  }
+});
+
+FlowRouter.route("/art", {
+  action() {
+    mount(MainLayout, {
+      content: <ArtList />
+    });
+  }
+});
+
+FlowRouter.route("/post", {
   action() {
     mount(MainLayout, {
       content: <Home />
---
+++ imports/ui/ArtList.jsx
@@ -0,0 +1,40 @@
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Pages } from '../api/pages.js';
+
+import Art from './Art.jsx';
+
+
+
+class ArtList extends Component {
+  renderArt(){
+    return this.props.arts.map((art)=>(
+      <Art key={art._id} art={art} />
+    ));
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <h1>Recent Art</h1>
+
+        <ul>
+          {this.renderArt()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+
+ArtList.propTypes = {
+  arts: PropTypes.array.isRequired
+};
+
+
+export default createContainer(()=>{
+  return {
+    arts: Pages.find({}).fetch()
+  };
+}, ArtList);
