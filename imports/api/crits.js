8649787f2d61f2a4ce77cfc350ef1ce13f62915f
dierat <diedra.rater@gmail.com>
2016-04-13 15:59:36 -0500

    adds crits and pages collections

---
+++ imports/api/crits.js
@@ -0,0 +1,5 @@
+import { Mongo } from 'meteor/mongo';
+
+
+
+export const Crits = new Mongo.Collection('crits');
---
+++ imports/api/pages.js
@@ -0,0 +1,5 @@
+import { Mongo } from 'meteor/mongo';
+
+
+
+export const Pages = new Mongo.Collection('pages');
