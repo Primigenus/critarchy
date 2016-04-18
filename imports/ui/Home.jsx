941ea0c7e8209de422540690f2ec41dd0df8312a
dierat <diedra.rater@gmail.com>
2016-04-18 15:49:02 -0700

    reafactors Home to use CritList and ArtList components for less repetition

--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -1,56 +1,21 @@
 import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 
-import { Crits } from '../api/crits.js';
-import { Pages } from '../api/pages.js';
-
-import Crit from './Crit.jsx';
-import Art from './Art.jsx';
+import CritList from './CritList.jsx';
+import ArtList from './ArtList.jsx';
 
 
 
-class Home extends Component {
-  renderCrits(){
-    return this.props.crits.map((crit)=>(
-      <Crit key={crit._id} crit={crit} />
-    ));
-  }
-
-  renderArt(){
-    return this.props.arts.map((art)=>(
-      <Art key={art._id} art={art} />
-    ));
-  }
-
+export default class Home extends Component {
   render(){
     return (
       <div className="container">
-        <h1>Top Crits</h1>
+        <CritList />
+        <a href="/crits">More Crits</a>
 
-        <ul>
-          {this.renderCrits()}
-        </ul>
-
-        <h1>Recent Art</h1>
-
-        <ul>
-          {this.renderArt()}
-        </ul>
+        <ArtList />
+        <a href="/art">More Art</a>
       </div>
     );
   }
 }
-
-
-Home.propTypes = {
-  crits: PropTypes.array.isRequired,
-  arts: PropTypes.array.isRequired,
-};
-
-
-export default createContainer(()=>{
-  return {
-    crits: Crits.find({}, {limit: 10}).fetch(),
-    arts: Pages.find({}, {limit: 10}).fetch(),
-  };
-}, Home);
