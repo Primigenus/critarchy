6ed16d4509bd579d110b81d120bbe2213be9e710
Rahul Choudhury <rchoudhury@gmail.com>
2016-12-23 22:38:55 -0800

    Refactor

---
+++ graphql-server/imports/api/parseJsonLiteral.js
@@ -0,0 +1,26 @@
+import Kind from 'graphql';
+
+const parseJSONLiteral = (ast) => {
+  switch(ast.kind) {
+    case Kind.STRING:
+    case Kind.BOOLEAN:
+      return ast.value;
+    case Kind.INT:
+    case Kind.FLOAT:
+      return parseFloat(ast.value);
+    case Kind.OBJECT: {
+      const value = Object.create(null);
+      ast.fields.forEach((field) => {
+        value[field.name.value] = parseJSONLiteral(field.value);
+      });
+
+      return value;
+    }
+    case Kind.LIST:
+      return ast.values.map(parseJSONLiteral);
+    default:
+      return null;
+  }
+};
+
+export default parseJSONLiteral;
--- graphql-server/imports/api/resolvers.js
+++ graphql-server/imports/api/resolvers.js
@@ -1,29 +1,5 @@
-import { Kind } from 'graphql';
-
 import uploadToGCS from './gcs-connector';
-
-function parseJSONLiteral(ast) {
-  switch(ast.kind) {
-    case Kind.STRING:
-    case Kind.BOOLEAN:
-      return ast.value;
-    case Kind.INT:
-    case Kind.FLOAT:
-      return parseFloat(ast.value);
-    case Kind.OBJECT: {
-      const value = Object.create(null);
-      ast.fields.forEach((field) => {
-        value[field.name.value] = parseJSONLiteral(field.value);
-      });
-
-      return value;
-    }
-    case Kind.LIST:
-      return ast.values.map(parseJSONLiteral);
-    default:
-      return null;
-  }
-}
+import parseJSONLiteral from './parseJsonLiteral';
 
 export default {
   Query: {
@@ -33,6 +9,9 @@ export default {
   },
   Mutation: {
     async uploadImage(root, { id, files }, { settings }) {
+      if(!files) {
+        throw new Error('No files to upload');
+      }
       const results = await uploadToGCS(id, files, settings);
       return results;
     },
