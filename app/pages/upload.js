024d13c090addb205a1f31021398b75038a2059a
Rahul Choudhury <rchoudhury@gmail.com>
2016-12-31 02:31:45 -0800

    List which files will be uploaded and disallow >5MB individual uploads

--- app/pages/upload.js
+++ app/pages/upload.js
@@ -16,11 +16,11 @@ export default layout('Upload', class UploadImage extends React.Component {
     super(props);
     this.state = {
       uploadedImages: null,
+      uploadingImages: null,
       uploading: false,
     };
   }
   async onSubmit(evt) {
-    // TODO: preview images to upload, prevent uploading if >=5MB or not right file type
     evt.preventDefault();
     this.setState({
       uploading: true,
@@ -38,21 +38,38 @@ export default layout('Upload', class UploadImage extends React.Component {
   }
   changeFiles(evt) {
     this.files = evt.target.files;
+
+    const arrayFiles = Array.from(this.files);
+    this.setState({
+      uploadTooBigError: arrayFiles.filter(f => f.size >= 5000000).length > 0,
+      uploadingImages: arrayFiles,
+    });
+  }
+  formatFileSize(size) {
+    // http://stackoverflow.com/a/20463021/16308
+    // eslint-disable-next-line
+    const fn = (a,b,c,d,e) => (b=Math,c=b.log,d=1e3,e=c(a)/c(d)|0,a/b.pow(d,e)).toFixed(2)+' '+(e?'kMGTPEZY'[--e]+'B':'Bytes');
+    return fn(size);
   }
   render() {
-    const { uploading, uploadedImages } = this.state;
+    const { uploading, uploadedImages, uploadingImages, uploadTooBigError } = this.state;
     return (
       <div>
         <form method="post" onSubmit={ evt => this.onSubmit(evt) } encType="multipart/form-data">
           { uploading ? <div className="loading">Uploading...</div> : <span /> }
           <p><input type="file" onChange={ evt => this.changeFiles(evt) } /></p>
-          <p><input type="submit" defaultValue="Upload" disabled={ !!uploading } /></p>
-          { uploadedImages
-            ? uploadedImages.map((url, i) => <p key={ i }>
+          <p><input type="submit" defaultValue="Upload" disabled={ !!uploading || !!uploadTooBigError } /></p>
+          { uploadTooBigError && <p>Please only select images smaller than 5MB.</p> }
+          { !uploadTooBigError && uploadingImages && uploadingImages
+            .map(({ name, size }, i) => <p key={ i }>
+              Image queued for upload: { name } ({ this.formatFileSize(size) }).
+            </p>)
+          }
+          {
+            uploadedImages && uploadedImages.map((url, i) => <p key={ i }>
               You uploaded this image!<br />
               <img src={ url } alt="" style={ { height: 300 } } />
             </p>)
-            : <span />
           }
         </form>
       </div>
