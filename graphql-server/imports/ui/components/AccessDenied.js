2cd29dd12ed7088b2991381f27de9bea238292f4
Rahul Choudhury <rchoudhury@gmail.com>
2017-02-13 17:08:06 -0800

    Remove old unused files

--- graphql-server/imports/ui/components/AccessDenied.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// import Link from 'next/prefetch';
-
-export default () => (
-  <div>
-    <h1>Access denied</h1>
-    <p>Please <a href="/login">log in</a> to access this page.</p>
-  </div>
-);
