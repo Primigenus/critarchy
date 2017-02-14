4e1bdac27ca080b9919e74c7127073b316bae241
Rahul Choudhury <rchoudhury@gmail.com>
2017-02-14 15:07:58 -0800

    Clean up header

--- graphql-server/imports/ui/components/Header.js
+++ graphql-server/imports/ui/components/Header.js
@@ -2,20 +2,22 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { Link } from 'react-router';
 
-const uploadLink = <li><Link to="/upload">Upload art</Link></li>;
-const sketchbookLink = <li><Link to="/sketchbook">Sketchbook</Link></li>;
-const signinLink = <li><Link to="/signin">Sign in</Link></li>;
-
 const Header = ({ hasUser, user }) => (
   <header>
     <Link to="/">Critarchy</Link>
     { hasUser && <span>logged in as { user.name }</span> }
     <nav>
-      <ul>
-        { hasUser && uploadLink }
-        { hasUser && sketchbookLink }
-        { !hasUser && signinLink }
-      </ul>
+      { hasUser ?
+        <ul>
+          <li><Link to="/upload">Upload art</Link></li>
+          <li><Link to="/sketchbook">Sketchbook</Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+        </ul>
+        :
+        <ul>
+          <li><Link to="/signin">Sign in</Link></li>
+        </ul>
+      }
     </nav>
   </header>
 );
