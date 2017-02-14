8d404beb3a391e3fea7e187696f04b67d13f1a7c
Rahul Choudhury <rchoudhury@gmail.com>
2017-02-14 15:07:15 -0800

    Set name & picture on user

--- graphql-server/imports/startup/server/accounts.js
+++ graphql-server/imports/startup/server/accounts.js
@@ -1,13 +1,19 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 
-Meteor.startup(function accountsServerStartup() {
-  Accounts.onCreateUser((user) => {
+Meteor.startup(() => {
+  Accounts.onCreateUser((opts, user) => {
     if(user.services) {
       if(user.services.facebook) {
-        Object.assign(user, { name: user.services.facebook.name });
+        Object.assign(user, {
+          name: user.services.facebook.first_name,
+          picture: `https://graph.facebook.com/${user.services.facebook.id}/picture`,
+        });
       } else if(user.services.google) {
-        Object.assign(user, { name: user.services.google.name });
+        Object.assign(user, {
+          name: user.services.google.given_name,
+          picture: user.services.google.picture,
+        });
       }
     }
     return user;
