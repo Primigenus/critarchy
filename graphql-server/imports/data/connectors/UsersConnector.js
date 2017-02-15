29e244e58758f4925cc0cc94a61c27e0e9cc6685
Rahul Choudhury <rchoudhury@gmail.com>
2017-02-15 13:57:17 -0800

    Remove redundant code

--- graphql-server/imports/data/connectors/UsersConnector.js
+++ graphql-server/imports/data/connectors/UsersConnector.js
@@ -1,14 +1,7 @@
-import jwtDecode from 'jwt-decode';
 import { Users } from '../collections';
 
 export default class UsersConnector {
-  static getUserFromToken(token) {
-    return jwtDecode(token);
-  }
   static async getById(id) {
     return await Users.findOne(id);
-  }
-  static async storeUser(user) {
-    return await Users.upsert({ user_id: user.user_id }, user);
   }
 }
