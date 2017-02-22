9f57ece06975393505366e8d1b2217bc7909ac65
Rahul Choudhury <rchoudhury@gmail.com>
2017-02-22 13:16:06 -0800

    Add userpicture component

--- graphql-server/imports/ui/components/UserPicture/index.js
+++ graphql-server/imports/ui/components/UserPicture/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import './style.css';
 
 const UserPicture = ({ picture, size = 'normal' }) => (
-  <img
-    className="user-picture"
-    src={ picture }
-    alt=""
-    style={ {
-      width: size === 'large' ? 40 : 20,
-      height: size === 'large' ? 40 : 20,
-    } }
-  />
+  <div className="user-picture">
+    <img
+      className="user-picture-img"
+      src={ picture }
+      alt=""
+      style={ {
+        width: size === 'large' ? 40 : 20,
+        height: size === 'large' ? 40 : 20,
+      } }
+    />
+  </div>
 );
+
+UserPicture.propTypes = {
+  picture: React.PropTypes.string.isRequired,
+  size: React.PropTypes.string.isRequired,
+};
 
 export default UserPicture;
