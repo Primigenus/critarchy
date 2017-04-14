e964fc2f193edb3a386f50d64815fab7a18a1577
Rahul Choudhury <rchoudhury@gmail.com>
2017-04-13 23:32:03 -0700

    Clean up header rendering

--- graphql-server/imports/ui/components/Header.js
+++ graphql-server/imports/ui/components/Header.js
@@ -2,22 +2,26 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { IndexLink, Link } from 'react-router';
 
+const SignedInNav = () => (
+  <ul>
+    <li><IndexLink to="/" activeClassName="active">Latest</IndexLink></li>
+    <li><Link to="/upload" className="upload-art" activeClassName="active">Upload</Link></li>
+    <li><Link to="/sketchbook" activeClassName="active">Sketchbook</Link></li>
+    <li><Link to="/profile" activeClassName="active">Profile</Link></li>
+  </ul>
+);
+
+const SignedOutNav = () => (
+  <ul>
+    <li><Link to="/signin" activeClassName="active">Sign in</Link></li>
+  </ul>
+);
+
 const Header = ({ hasUser }) => (
   <header className="main-header">
     <div className="logo"><Link to="/">Critarchy</Link></div>
     <nav className="main-nav">
-      { hasUser ?
-        <ul>
-          <li><IndexLink to="/" activeClassName="active">Latest</IndexLink></li>
-          <li><Link to="/upload" className="upload-art" activeClassName="active">Upload</Link></li>
-          <li><Link to="/sketchbook" activeClassName="active">Sketchbook</Link></li>
-          <li><Link to="/profile" activeClassName="active">Profile</Link></li>
-        </ul>
-        :
-        <ul>
-          <li><Link to="/signin" activeClassName="active">Sign in</Link></li>
-        </ul>
-      }
+      { hasUser ? <SignedInNav /> : <SignedOutNav /> }
     </nav>
   </header>
 );
