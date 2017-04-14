193326811b13ec02679994f6caf6752f3adf480e
Rahul Choudhury <rchoudhury@gmail.com>
2017-04-13 23:31:53 -0700

    Disable manual reducer

--- graphql-server/imports/startup/client/apollo.js
+++ graphql-server/imports/startup/client/apollo.js
@@ -1,13 +1,12 @@
 import { meteorClientConfig } from 'meteor/apollo'; // eslint-disable-line
-
 import ApolloClient from 'apollo-client';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+// import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 export const client = new ApolloClient(meteorClientConfig());
 
-export const store = createStore(
-  combineReducers({
-    apollo: client.reducer(),
-  }),
-  applyMiddleware(client.middleware()),
-);
+// export const store = createStore(
+//   combineReducers({
+//     apollo: client.reducer(),
+//   }),
+//   applyMiddleware(client.middleware()),
+// );
