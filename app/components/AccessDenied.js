24456124f8de137c0dbbe7d2c54a0a591fd10492
Rahul Choudhury <rchoudhury@gmail.com>
2017-04-28 17:23:57 -0700

    Remove old nextjs app

--- app/components/AccessDenied.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Link from 'next/prefetch';
-
-export default () => (
-  <div>
-    <h1>Access denied</h1>
-    <p>Please <Link href='/login'><a>log in</a></Link> to access this page.</p>
-  </div>
-);
--- app/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer>
-    <span>&copy; Critarchy Authors, 2017</span>
-  </footer>
-);
-
-export default Footer;
