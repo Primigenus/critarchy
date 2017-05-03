020005d0fcd3886525dad37eaf0a27754497f1ff
Rahul Choudhury <rchoudhury@gmail.com>
2017-05-02 21:21:48 -0700

    Log errors

--- src/imports/ui/components/Loading.js
+++ src/imports/ui/components/Loading.js
@@ -4,6 +4,7 @@ export default ({ isLoading, error, pastDelay }: Props) => {
   if (isLoading) {
     return pastDelay ? <div>Loading...</div> : null; // Don't flash "Loading..." when we don't need to.
   } else if (error) {
+    console.error(error);
     return <div>Error! Component failed to load</div>;
   } else {
     return null;
